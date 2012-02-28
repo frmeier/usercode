38ad2ab43295a4376e29d63cc95f0819ca0bb3bd
frmeier <>
2012-02-28 13:05:24 +0000

    Files to document privately produced MC samples heading for central production

---
+++ privateMCproduction/PYTHIA6_exclLambdaBtoJpsiMuMu_7TeV_cff_py_GEN_SIM_DIGI_L1_DIGI2RAW.py
@@ -0,0 +1,156 @@
+# Auto generated configuration file
+# using: 
+# Revision: 1.303.2.7 
+# Source: /cvs_server/repositories/CMSSW/CMSSW/Configuration/PyReleaseValidation/python/ConfigBuilder.py,v 
+# with command line options: Configuration/GenProduction/python/PYTHIA6_B0ToJpsiKs_7TeV_EvtGen_Frank_cff.py --step GEN,SIM,DIGI,L1,DIGI2RAW --beamspot Realistic7TeV2011Collision --conditions START42_V14A::All --pileup NoPileUp --datamix NODATAMIXER --eventcontent RAWSIM --datatier GEN-SIM-RAW --no_exec
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('DIGI2RAW')
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('SimGeneral.MixingModule.mixNoPU_cfi')
+process.load('Configuration.StandardSequences.GeometryDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_38T_cff')
+process.load('Configuration.StandardSequences.Generator_cff')
+process.load('IOMC.EventVertexGenerators.VtxSmearedRealistic7TeV2011Collision_cfi')
+process.load('GeneratorInterface.Core.genFilterSummary_cff')
+process.load('Configuration.StandardSequences.SimIdeal_cff')
+process.load('Configuration.StandardSequences.Digi_cff')
+process.load('Configuration.StandardSequences.SimL1Emulator_cff')
+process.load('Configuration.StandardSequences.DigiToRaw_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(1)
+)
+
+# Input source
+process.source = cms.Source("EmptySource")
+
+process.options = cms.untracked.PSet(
+
+)
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.9 $'),
+    annotation = cms.untracked.string('Summer09: Pythia6+EvtGen generation of B0->Jpsi->MuMu, 7TeV, D6T tune'),
+    name = cms.untracked.string('$Source: /cvs/CMSSW/CMSSW/Configuration/GenProduction/python/PYTHIA6_inclB0toJpsiMuMu_7TeV_cff.py,v $')
+)
+
+# Output definition
+
+process.RAWSIMoutput = cms.OutputModule("PoolOutputModule",
+    splitLevel = cms.untracked.int32(0),
+    eventAutoFlushCompressedSize = cms.untracked.int32(5242880),
+    outputCommands = process.RAWSIMEventContent.outputCommands,
+    fileName = cms.untracked.string('PYTHIA6_B0ToJpsiKs_7TeV_EvtGen_Frank_cff_py_GEN_SIM_DIGI_L1_DIGI2RAW.root'),
+    dataset = cms.untracked.PSet(
+        filterName = cms.untracked.string(''),
+        dataTier = cms.untracked.string('GEN-SIM-RAW')
+    ),
+    SelectEvents = cms.untracked.PSet(
+        SelectEvents = cms.vstring('generation_step')
+    )
+)
+
+# Additional output definition
+
+# Other statements
+process.GlobalTag.globaltag = 'START42_V14A::All'
+
+process.oniafilter = cms.EDFilter("MCSingleParticleFilter",
+    MaxEta = cms.untracked.vdouble(1000.0, 1000.0),
+    Status = cms.untracked.vint32(2, 2),
+    MinEta = cms.untracked.vdouble(-1000.0, -1000.0),
+    MinPt = cms.untracked.vdouble(0.0, 0.0),
+    ParticleID = cms.untracked.vint32(443, 100443)
+)
+
+
+process.b0filter = cms.EDFilter("PythiaFilter",
+    ParticleID = cms.untracked.int32(511)
+)
+
+
+process.generator = cms.EDFilter("Pythia6GeneratorFilter",
+    ExternalDecays = cms.PSet(
+        EvtGen = cms.untracked.PSet(
+            use_default_decay = cms.untracked.bool(False),
+            decay_table = cms.FileInPath('GeneratorInterface/ExternalDecays/data/DECAY_NOLONGLIFE.DEC'),
+            particle_property_file = cms.FileInPath('GeneratorInterface/ExternalDecays/data/evtLbB0Pdg11.pdl'),
+            user_decay_file = cms.FileInPath('GeneratorInterface/ExternalDecays/data/B0ToJpsiKs.dec'),
+            list_forced_decays = cms.vstring('MyB0', 
+                'Myanti-B0'),
+            operates_on_particles = cms.vint32(0)
+        ),
+        parameterSets = cms.vstring('EvtGen')
+    ),
+    pythiaPylistVerbosity = cms.untracked.int32(0),
+    filterEfficiency = cms.untracked.double(0.00129),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    comEnergy = cms.double(7000.0),
+    crossSection = cms.untracked.double(26500000.0),
+    maxEventsToPrint = cms.untracked.int32(0),
+    PythiaParameters = cms.PSet(
+        pythiaUESettings = cms.vstring('MSTU(21)=1     ! Check on possible errors during program execution', 
+            'MSTJ(22)=2     ! Decay those unstable particles', 
+            'PARJ(71)=10 .  ! for which ctau  10 mm', 
+            'MSTP(33)=0     ! no K factors in hard cross sections', 
+            'MSTP(2)=1      ! which order running alphaS', 
+            'MSTP(51)=10042 ! structure function chosen (external PDF CTEQ6L1)', 
+            'MSTP(52)=2     ! work with LHAPDF', 
+            'PARP(82)=1.832 ! pt cutoff for multiparton interactions', 
+            'PARP(89)=1800. ! sqrts for which PARP82 is set', 
+            'PARP(90)=0.275 ! Multiple interactions: rescaling power', 
+            'MSTP(95)=6     ! CR (color reconnection parameters)', 
+            'PARP(77)=1.016 ! CR', 
+            'PARP(78)=0.538 ! CR', 
+            'PARP(80)=0.1   ! Prob. colored parton from BBR', 
+            'PARP(83)=0.356 ! Multiple interactions: matter distribution parameter', 
+            'PARP(84)=0.651 ! Multiple interactions: matter distribution parameter', 
+            'PARP(62)=1.025 ! ISR cutoff', 
+            'MSTP(91)=1     ! Gaussian primordial kT', 
+            'PARP(93)=10.0  ! primordial kT-max', 
+            'MSTP(81)=21    ! multiple parton interactions 1 is Pythia default', 
+            'MSTP(82)=4     ! Defines the multi-parton model'),
+        bbbarSettings = cms.vstring('MSEL = 1'),
+        parameterSets = cms.vstring('pythiaUESettings', 
+            'bbbarSettings')
+    )
+)
+
+
+process.mumugenfilter = cms.EDFilter("MCParticlePairFilter",
+    Status = cms.untracked.vint32(1, 1),
+    MaxEta = cms.untracked.vdouble(2.5, 2.5),
+    MinEta = cms.untracked.vdouble(-2.5, -2.5),
+    ParticleCharge = cms.untracked.int32(-1),
+    MinP = cms.untracked.vdouble(2.5, 2.5),
+    ParticleID1 = cms.untracked.vint32(13),
+    ParticleID2 = cms.untracked.vint32(13)
+)
+
+
+process.ProductionFilterSequence = cms.Sequence(process.generator+process.b0filter+process.oniafilter+process.mumugenfilter)
+
+# Path and EndPath definitions
+process.generation_step = cms.Path(process.pgen)
+process.simulation_step = cms.Path(process.psim)
+process.digitisation_step = cms.Path(process.pdigi)
+process.L1simulation_step = cms.Path(process.SimL1Emulator)
+process.digi2raw_step = cms.Path(process.DigiToRaw)
+process.genfiltersummary_step = cms.EndPath(process.genFilterSummary)
+process.endjob_step = cms.EndPath(process.endOfProcess)
+process.RAWSIMoutput_step = cms.EndPath(process.RAWSIMoutput)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.generation_step,process.genfiltersummary_step,process.simulation_step,process.digitisation_step,process.L1simulation_step,process.digi2raw_step,process.endjob_step,process.RAWSIMoutput_step)
+# filter all path with the production filter sequence
+for path in process.paths:
+	getattr(process,path)._seq = process.ProductionFilterSequence * getattr(process,path)._seq 
---
+++ privateMCproduction/PYTHIA6_exclLambdaBtoJpsiMuMu_7TeV_cff_py_GEN_SIM_DIGI_L1_DIGI2RAW_root_HLT_RAW2DIGI_L1Reco_RECO.py
@@ -0,0 +1,126 @@
+# Auto generated configuration file
+# using: 
+# Revision: 1.303.2.7 
+# Source: /cvs_server/repositories/CMSSW/CMSSW/Configuration/PyReleaseValidation/python/ConfigBuilder.py,v 
+# with command line options: PYTHIA6_B0ToJpsiKs_7TeV_EvtGen_Frank_cff_py_GEN_SIM_DIGI_L1_DIGI2RAW.root --step HLT:BPH,RAW2DIGI,L1Reco,RECO --beamspot Realistic7TeV2011Collision --conditions START42_V14A::All --pileup NoPileUp --datamix NODATAMIXER --eventcontent RECOSIM --datatier GEN-SIM-RECO --no_exec
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('HLT')
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('SimGeneral.MixingModule.mixNoPU_cfi')
+process.load('Configuration.StandardSequences.GeometryDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_38T_cff')
+process.load('HLTrigger.Configuration.HLT_quarkonium_1E33_3E33_cff')
+process.load('Configuration.StandardSequences.RawToDigi_cff')
+process.load('Configuration.StandardSequences.L1Reco_cff')
+process.load('Configuration.StandardSequences.Reconstruction_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(1)
+)
+
+# Input source
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring('PYTHIA6_exclLambdaBtoJpsiMuMu_7TeV_cff_py_GEN_SIM_DIGI_L1_DIGI2RAW.root')
+)
+
+process.options = cms.untracked.PSet(
+
+)
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.303.2.7 $'),
+    annotation = cms.untracked.string('PYTHIA6_exclLambdaBtoJpsiMuMu_7TeV_cff_py_GEN_SIM_DIGI_L1_DIGI2RAW.root nevts:1'),
+    name = cms.untracked.string('PyReleaseValidation')
+)
+
+# Output definition
+
+process.RECOSIMoutput = cms.OutputModule("PoolOutputModule",
+    splitLevel = cms.untracked.int32(0),
+#eventAutoFlushCompressedSize = cms.untracked.int32(5242880),
+    outputCommands = process.RECOSIMEventContent.outputCommands,
+    fileName = cms.untracked.string('PYTHIA6_exclLambdaBtoJpsiMuMu_7TeV_cff_py_GEN_SIM_DIGI_L1_DIGI2RAW_root_HLT_quarkoniumRAW2DIGI_L1Reco_RECO.root'),
+    dataset = cms.untracked.PSet(
+        filterName = cms.untracked.string(''),
+        dataTier = cms.untracked.string('GEN-SIM-RECO')
+    )
+)
+
+# HLT stuff from https://espace.cern.ch/cms-quarkonia/onia-polarization/Generating%20Events/Detector%20simulation%20for%201E33.aspx
+process.GlobalTag.toGet.append(
+    cms.PSet(
+        record  = cms.string( 'L1MuGMTParametersRcd' ),
+        tag     = cms.string( 'L1MuGMTParameters_synctf_10_mc' ),
+        label   = cms.untracked.string( '' ),
+        connect = cms.untracked.string('frontier://FrontierProd/CMS_COND_31X_L1T')
+    ))
+process.GlobalTag.toGet.append(
+    cms.PSet(
+        record  = cms.string( 'L1MuDTTFParametersRcd' ),
+        tag     = cms.string( 'L1MuDTTFParameters_dttf11_TSC_09_17_col_mc' ),
+        label   = cms.untracked.string( '' ),
+        connect = cms.untracked.string('frontier://FrontierProd/CMS_COND_31X_L1T')
+        ))
+process.GlobalTag.toGet.append(
+    cms.PSet(
+        record  = cms.string( 'L1MuCSCTFConfigurationRcd' ),
+        tag     = cms.string( 'L1MuCSCTFConfiguration_90511_mc' ),
+        label   = cms.untracked.string( '' ),
+        connect = cms.untracked.string('frontier://FrontierProd/CMS_COND_31X_L1T')
+        ))
+process.GlobalTag.toGet.append(
+    cms.PSet(
+        record  = cms.string( 'L1RPCBxOrConfigRcd' ),
+        tag     = cms.string( 'L1RPCBxOrConfig_LHC7_1EX_mc' ),
+        label   = cms.untracked.string( '' ),
+        connect = cms.untracked.string('frontier://FrontierProd/CMS_COND_31X_L1T')
+        ))
+process.GlobalTag.toGet.append(
+    cms.PSet(
+        record  = cms.string( 'L1RPCConeDefinitionRcd' ),
+        tag     = cms.string( 'L1RPCConeDefinition_LHC7_1EX_mc' ),
+        label   = cms.untracked.string( '' ),
+        connect = cms.untracked.string('frontier://FrontierProd/CMS_COND_31X_L1T')
+        ))
+process.GlobalTag.toGet.append(
+    cms.PSet(
+        record  = cms.string( 'L1RPCConfigRcd' ),
+        tag     = cms.string( 'L1RPCConfig_LHC7_1EX_mc' ),
+        label   = cms.untracked.string( '' ),
+        connect = cms.untracked.string('frontier://FrontierProd/CMS_COND_31X_L1T')
+        ))
+process.GlobalTag.toGet.append(
+    cms.PSet(
+        record  = cms.string( 'L1RPCHsbConfigRcd' ),
+        tag     = cms.string( 'L1RPCHsbConfig_LHC7_1EX_mc' ),
+        label   = cms.untracked.string( '' ),
+        connect = cms.untracked.string('frontier://FrontierProd/CMS_COND_31X_L1T')
+        )
+    )
+
+# Additional output definition
+
+# Other statements
+process.GlobalTag.globaltag = 'START42_V14A::All'
+
+# Path and EndPath definitions
+process.raw2digi_step = cms.Path(process.RawToDigi)
+process.L1Reco_step = cms.Path(process.L1Reco)
+process.reconstruction_step = cms.Path(process.reconstruction)
+process.endjob_step = cms.EndPath(process.endOfProcess)
+process.RECOSIMoutput_step = cms.EndPath(process.RECOSIMoutput)
+
+# Schedule definition
+process.schedule = cms.Schedule()
+process.schedule.extend(process.HLTSchedule)
+process.schedule.extend([process.raw2digi_step,process.L1Reco_step,process.reconstruction_step,process.endjob_step,process.RECOSIMoutput_step])
+
